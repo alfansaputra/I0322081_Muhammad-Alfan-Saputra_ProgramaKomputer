1e2d5b9914ae12434a7ce5ab436bc180792c3af3
alfansaputra <127107956+alfansaputra@users.noreply.github.com>
2023-03-13 17:51:15 +0700

    Add files via upload

---
+++ exercise-01.py
@@ -0,0 +1,12 @@
+nama = "Muhammad alfan saputra"
+NIM = "I0322081"
+kampunghalaman ="Pemalang"
+visualisasi ="Rumahku memepunyai taman yang luas serta banyak tanaman di sekeliling rumah dan rumahclsku dekat dengan sungai"
+harapansetelahlulus ="Menajadi orang yang bisa bermanfaaat untuk sekitar"
+
+print(nama)
+print(NIM)
+print(kampunghalaman)
+print(visualisasi)
+print(harapansetelahlulus)
+
